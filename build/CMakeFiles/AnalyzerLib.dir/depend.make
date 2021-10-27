195c01873322607982dcbe483289a2fa33f0418e
Jakub Tederko <exoil@jakubs-macbook-air.home>
2021-10-27 16:48:53 +0200

    removed build folder

--- build/CMakeFiles/AnalyzerLib.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for AnalyzerLib.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/exoil/Documents/Dev/Cpp/DirectoryAnalyzer
-# Build directory: /Users/exoil/Documents/Dev/Cpp/DirectoryAnalyzer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AllTestsInFoo "foo")
-set_tests_properties(AllTestsInFoo PROPERTIES  _BACKTRACE_TRIPLES "/Users/exoil/Documents/Dev/Cpp/DirectoryAnalyzer/CMakeLists.txt;40;add_test;/Users/exoil/Documents/Dev/Cpp/DirectoryAnalyzer/CMakeLists.txt;0;")
